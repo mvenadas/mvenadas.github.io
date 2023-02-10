5760e1cbffd6bd02984b53824c712c812f63e160
Michael Venadas <31321282+mvenadas@users.noreply.github.com>
2023-02-10 14:07:22 -0800

    Add files via upload

--- js/animate.js
+++ js/animate.js
@@ -29,14 +29,74 @@ gsap.to(".position-title", {
     opacity: 9, 
 });
 
+gsap.to(".bubbs", {
+    opacity: 9,
+    duration: 0.5,
+    delay: 1.5,
+    y: 600
+});
+
+// Sections - Animation
+
+gsap.from("#about-me", {
+    scrollTrigger: {
+      trigger: "#about-me",
+      start: "25px bottom",
+      markers: true
+
+    },
+    x: -1500,
+    ease: "power4.inOut",
+    duration: 1.5    
+});
+
+gsap.from("#portfolio", {
+  scrollTrigger: {
+    trigger: "#portfolio",
+    start: "25px bottom",
+    markers: true
+  },
+  x: 1500,
+  ease: "power4.inOut",
+  duration: 1.5   
+});
+
+gsap.from("#contact-me", {
+  scrollTrigger: {
+    trigger: "#contact-me",
+    start: "25px bottom",
+    markers: true
+  },
+  x: -1500,
+  ease: "power4.inOut",
+  duration: 1.5   
+});
+
 // Mobile
 
 ScrollTrigger.matchMedia({
-    "(max-width: 699px)": () => {
+    "(max-width: 992px)": () => {
       gsap.to('.name-box h1', {x: -100,stagger: 0,});
-    }, 
-    "(min-width: 700px)": () => {
-      gsap.to('h1', {color: 'green'});
-    }, 
+
+      gsap.to(".bubbs", {
+        opacity: 9,
+        duration: 0.5,
+        delay: 1.5,
+        y: 80
+    });
+
+
+
+
+    }
+
+
+
+
+
+    // "(min-width: 700px)": () => {
+    //   gsap.to('h1', {color: 'green'});
+    // }, 
+
   });
   
