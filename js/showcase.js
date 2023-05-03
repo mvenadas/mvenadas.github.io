f6928ef4d7177c2f9f1a68e19960d517af5e3407
Michael Venadas <31321282+mvenadas@users.noreply.github.com>
2023-05-03 13:25:49 -0700

    updates

--- js/showcase.js
+++ js/showcase.js
@@ -1,21 +1,79 @@
-// function reveal() {
-//   document.getElementById("content").style.display = "initial";
+// GSAP Panel Animation
+function gsapAni() {
+  gsap.from("#content", {
+    x: 500,
+    duration: 0.5,
+    ease: "power3.out",
+  });
 
-//   gsap.from("#content", {
-//     x: 500,
-//     duration: 0.5,
-//     ease: "power3.out",
-//   });
+  gsap.from(".closeBtn img", {
+    delay: 1,
+    y: -100,
+    ease: "power3.out",
+  });
 
-//   gsap.from(".closeBtn img", {
-//     delay: 1,
-//     y: -100,
-//     ease: "power3.out",
-//   });
+  document.getElementById("pscoBtn").style.pointerEvents = "none";
+  document.getElementById("agBtn").style.pointerEvents = "none";
+  document.getElementById("talBtn").style.pointerEvents = "none";
+  document.getElementById("thgBtn").style.pointerEvents = "none";
+}
 
-//   console.log("test");
-// }
+function showContent() {
+  document.getElementById("content").style.display = "block";
+}
 
-// document.querySelector("#pscoBtn").addEventListener("click", reveal);
+function hideTal() {
+  document.getElementById("talContent").style.display = "none";
+}
 
-// console.log("mikey");
+function hideThg() {
+  document.getElementById("thgContent").style.display = "none";
+}
+
+const psco = {
+  pscoPanel: function () {
+    showContent();
+    document.getElementById("pscoContent").style.display = "block";
+    hideTal();
+    hideThg();
+    gsapAni();
+  },
+};
+
+const ag = {
+  agPanel: function () {
+    showContent();
+    document.getElementById("agContent").style.display = "block";
+    hideTal();
+    hideThg();
+    gsapAni();
+  },
+};
+
+const ncbm = {
+  ncbmPanel: function () {
+    showContent();
+    document.getElementById("ncbmContent").style.display = "block";
+    hideTal();
+    hideThg();
+    gsapAni();
+  },
+};
+
+const tal = {
+  talPanel: function () {
+    showContent();
+    document.getElementById("talContent").style.display = "block";
+    hideThg();
+    gsapAni();
+  },
+};
+
+const thg = {
+  thgPanel: function () {
+    showContent();
+    document.getElementById("thgContent").style.display = "block";
+    hideTal();
+    gsapAni();
+  },
+};
